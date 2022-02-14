df836b272a17bc663ef0ad947a7decc5b8a40232
Sebastian Medina <39243497+sebapch@users.noreply.github.com>
2022-02-14 18:15:38 -0300

    Add layout

---
+++ guuru/src/components/Layout/Layout.jsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+import Navbar from '../Navbar/Navbar';
+import Footer from '../Footer/Footer';
+
+const Layout = () => {
+    return (
+        <>
+            <Navbar />
+          
+            <Footer />
+        </>
+    )
+}
+
+export default Layout
